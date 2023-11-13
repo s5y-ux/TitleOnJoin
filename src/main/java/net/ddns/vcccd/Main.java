4840042f1864130d9ca29ff9ac2b5cbc6ffeb620
Joseph Shumaker <59636597+s5y-ux@users.noreply.github.com>
2023-11-12 22:47:16 -0800

    Add files via upload

---
+++ src/main/java/net/ddns/vcccd/Main.java
@@ -0,0 +1,43 @@
+package net.ddns.vcccd;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	ConsoleCommandSender console = getServer().getConsoleSender();
+	
+	@Override
+	public void onEnable() {
+		
+		//Used to get the config.yml file from the server folder
+		FileConfiguration config = this.getConfig();
+		
+		//Adds the default configuration options to the plugin
+		config.addDefault("TitleOnJoin", "&6&lWelcome %PlayerName%");
+		config.addDefault("SubTitleOnJoin", "To Our Server!");
+		config.addDefault("Particles", false);
+		config.addDefault("ParticleType", "Clouds");
+		config.addDefault("Enter", 3);
+		config.addDefault("Stay", 60);
+		config.addDefault("Exit", 3);
+		
+		//Saves the configuration file
+		this.saveDefaultConfig();
+		
+		//Registers the player join event defined in OnPlayerJoin();
+		getServer().getPluginManager().registerEvents(new OnPlayerJoin(this), this);
+		
+		//Woo hoo no errors!
+		console.sendMessage(ChatColor.GREEN + "Title on join working as expected :)");
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
---
+++ src/main/java/net/ddns/vcccd/OnPlayerJoin.java
@@ -0,0 +1,93 @@
+package net.ddns.vcccd;
+
+import java.util.HashMap;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class OnPlayerJoin implements Listener {
+
+    //Used to store the main body program for config reference
+    private final Main main;
+
+    //Constructor
+    public OnPlayerJoin(Main main) {
+
+        //Used to set instance main to main as passed in the Main class
+        this.main = main;
+    }
+
+    @EventHandler
+    public void OnJoin(PlayerJoinEvent event) {
+
+        //HashMap used to reference the Title and Sub Title
+        HashMap < String, String > PlaceHolders = new HashMap < String, String > ();
+
+        //Used to reference the config file in the program
+        FileConfiguration config = main.getConfig();
+
+        //Used to reference the player in the Join event
+        Player player = event.getPlayer();
+
+        //Placeholder hash-map, will replace later with more place holders
+        PlaceHolders.put("%PlayerName%", player.getName());
+
+        //Strings to pass onto the send title method as defined by the config
+        String PassValueOne = config.getString("TitleOnJoin");
+        String PassValueTwo = config.getString("SubTitleOnJoin");
+
+        //Used to replace all of the regex held in the placeholder HashMap
+        for (String PlaceHolderValues: PlaceHolders.keySet()) {
+            PassValueOne = PassValueOne.replaceAll(PlaceHolderValues, PlaceHolders.get(PlaceHolderValues));
+            PassValueTwo = PassValueTwo.replaceAll(PlaceHolderValues, PlaceHolders.get(PlaceHolderValues));
+        }
+
+        //Used to translate formatting codes from the standard Unicode Character
+        String title = ChatColor.translateAlternateColorCodes('&', PassValueOne);
+        String subtitle = ChatColor.translateAlternateColorCodes('&', PassValueTwo);
+
+        //Send the player subtitles
+        player.sendTitle(title, subtitle, config.getInt("Enter"), config.getInt("Stay"), config.getInt("Exit"));
+
+        //Gets the player location
+        Location PlayerLocation = player.getLocation();
+
+
+        //Creates the HashMap for the particles
+        HashMap < String, Particle > ParticleList = new HashMap < String, Particle > ();
+
+        //Adds the particles to the map
+        ParticleList.put("Clouds", Particle.CLOUD);
+        ParticleList.put("Hearts", Particle.HEART);
+        ParticleList.put("Spark", Particle.FIREWORKS_SPARK);
+        ParticleList.put("Soul", Particle.SOUL_FIRE_FLAME);
+
+        //Checks to see if we have the particles enabled in the config
+        if (config.getBoolean("Particles")) {
+
+            //Checks to see if it was configured correctly
+            if (ParticleList.keySet().contains(config.getString("ParticleType"))) {
+
+                //If so we spawn the particles
+                for (int index = -5; index < 5; index++) {
+                    player.getWorld().spawnParticle(ParticleList.get(config.getString("ParticleType")),
+                        PlayerLocation.getX(), PlayerLocation.getY(), PlayerLocation.getZ(), 10);
+                }
+
+                //Otherwise we tell the console there is a problem...
+            } else {
+                main.console.sendMessage(ChatColor.RED + "Check to see if the particles in the console were spelled right...");
+            }
+        }
+
+
+    }
+
+}
